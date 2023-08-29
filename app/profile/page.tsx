f255f122c65bd4d4dbf8b3dae44bae3b40c560a0
Ali Najdi <ali.najdi.1989@gmail.com>
2023-08-29 21:26:15 +0330

    added assets and initialized pages

---
+++ app/profile/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './profile.module.css'
+
+export default function Profile() {
+  return (
+    <main className={styles.main}>
+    profile
+    </main>
+  )
+}
---
+++ app/signUp/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './profile.module.css'
+
+export default function SignUp() {
+  return (
+    <main className={styles.main}>
+    sign up
+    </main>
+  )
+}
