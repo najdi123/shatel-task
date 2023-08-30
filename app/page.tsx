a4c8fb0e6c1f3b15a03be68aa9466f700d1ee0e2
Ali Najdi <ali.najdi.1989@gmail.com>
2023-08-30 17:51:31 +0330

    added whyFiber component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Header from '../ui/landingComponents/header/header'
+import WhyFiber from '../ui/landingComponents/whyFiber/whyFiber'
+
 
 export default function Home() {
   return (
@@ -8,6 +10,7 @@ export default function Home() {
     //  className={styles.main}
      >
       <Header/>
+      <WhyFiber/>
     </main>
   )
 }
