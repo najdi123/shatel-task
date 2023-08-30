f5a8fc67bf7cb442e77e2a9d71457c408ce7822c
Ali Najdi <ali.najdi.1989@gmail.com>
2023-08-30 16:22:52 +0330

    added rest of elements

--- ui/landingComponents/header/header.tsx
+++ ui/landingComponents/header/header.tsx
@@ -1,21 +1,53 @@
 import React from 'react'
 import styles from './headerStyles.module.css'
-// import Image from 'next/image'
+import Image from 'next/image'
 import Button from '../../button/Button'
 import Link from 'next/link'
+import Star from "../../../public/Assets/star.svg";
+import Checkmark from "../../../public/Assets/Checkmark.svg";
 
 export default function Header() {
   return (
     <section className={styles.container}>
         <div className={styles.content}>
-            <h1>Create your portfolio in minutes.</h1>
-            <p>
+        <div className={styles.rateWrapper}>
+            <div className={styles.starsWrapper}>
+            <Image src={Star} alt={"star"}/>
+            <Image src={Star} alt={"star"}/>
+            <Image src={Star} alt={"star"}/>
+            <Image src={Star} alt={"star"}/>
+            <Image src={Star} alt={"star"}/>
+            </div>
+            <p className={styles.rateText}>Rated 4.8/5 (243)reviews</p>
+        </div>
+            <h1 className={styles.title}>Create your portfolio in minutes.</h1>
+            <p className={styles.description}>
                 With Fiber, you can set up your own personal portfolio
                 in minutes with dozens of premade, beautiful templates.
             </p>
             <div className={styles.actionsWrapper}>
                 <Button>Start Free Trial</Button>
-                <Link href="/" >View examples</Link>
+                <Link className={styles.link} href="/" >View examples</Link>
+            </div>
+            <div className={styles.freeItemsWrapper}> 
+                <div className={styles.freeItem}>
+                <Image
+                     src={Checkmark}
+                      className={styles.icon}
+                      alt='checkmark'
+                      />
+                    <p className={styles.freeItemDescription}>No Credit Card Required</p>
+                </div>
+                <div className={styles.freeItem}>
+                    <Image
+                     src='/Assets/Checkmark.svg'
+                      className={styles.icon}
+                      width={'25'}
+                      height={'25'}
+                      alt='checkmark'
+                      />
+                    <p className={styles.freeItemDescription}>10 Free Templates</p>
+                </div>
             </div>
         </div>
         <div className={styles.imageWrapper}>
